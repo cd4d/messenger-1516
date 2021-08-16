af9669ec449e03c19b0fe1ab0cc095f958cad764
cd4d <11580810+cd4d@users.noreply.github.com>
2021-08-16 18:41:03 -0400

    Basic setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,14 @@
+require("dotenv").config()
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+  "admin_messenger",
+  process.env.DB_PASSWORD,
+  {
+    logging: false,
+    dialect: "postgres"
+  }
+);
 
 module.exports = db;
