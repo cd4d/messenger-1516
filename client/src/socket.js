7809acd455c203423c059654ad5eb28c33929778
cd4d <11580810+cd4d@users.noreply.github.com>
2021-08-16 22:58:50 -0400

    Sort messages by date

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
-import io from "socket.io-client";
+import {io} from "socket.io-client";
 import store from "./store";
 import {
   setNewMessage,
@@ -19,6 +19,7 @@ socket.on("connect", () => {
     store.dispatch(removeOfflineUser(id));
   });
   socket.on("new-message", (data) => {
+    console.log("socket data", data);
     store.dispatch(setNewMessage(data.message, data.sender));
   });
 });
