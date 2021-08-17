49b72d09ddbf5f2d2dc37fb492978326df548710
cd4d <11580810+cd4d@users.noreply.github.com>
2021-08-17 14:18:53 -0400

    Fix msgs not appearing 1

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -92,7 +92,7 @@ const sendMessage = (data, body) => {
 };
 
 // message format to send: {recipientId, text, conversationId}
-// conversationId will be set to null if its a brand new conversation
+// conversationId will be set to null if it's a brand new conversation
 export const postMessage = (body) => async (dispatch) => {
   try {
     const data = await saveMessage(body);
