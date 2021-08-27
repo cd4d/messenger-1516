3f4e4884738856e5e02c2ef320115f45091e7011
cd4d <11580810+cd4d@users.noreply.github.com>
2021-08-27 13:30:15 -0400

    Minor tweaks

--- client/src/components/Sidebar/CurrentUser.js
+++ client/src/components/Sidebar/CurrentUser.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
 import { BadgeAvatar } from "./index";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
@@ -11,25 +10,25 @@ const useStyles = makeStyles(() => ({
     marginTop: 23,
     marginLeft: 6,
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
   },
   subContainer: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    flexGrow: 1
+    flexGrow: 1,
   },
   username: {
     letterSpacing: -0.23,
     fontSize: 16,
     fontWeight: "bold",
-    marginLeft: 17
+    marginLeft: 17,
   },
   ellipsis: {
     color: "#95A7C4",
     marginRight: 24,
-    opacity: 0.5
-  }
+    opacity: 0.5,
+  },
 }));
 
 const CurrentUser = (props) => {
@@ -48,6 +47,4 @@ const CurrentUser = (props) => {
   );
 };
 
-
-
-export default connect(null)(CurrentUser);
+export default CurrentUser;
